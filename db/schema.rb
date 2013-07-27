af3c0f3da09e227dd666d262ff814494851920fa
Gabriel Ortega <gabriel.ortega56@gmail.com>
2013-07-27 15:16:22 -0400

    Generate Tag model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130727191104) do
+ActiveRecord::Schema.define(version: 20130727191431) do
 
   create_table "comments", force: true do |t|
     t.integer  "user_id"
@@ -30,6 +30,12 @@ ActiveRecord::Schema.define(version: 20130727191104) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "address"
+  end
+
+  create_table "tags", force: true do |t|
+    t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   create_table "users", force: true do |t|
