0f560b8c5d9f0edbd29ce65ce32a2ff8c46456f3
Gabriel Ortega <gabriel.ortega56@gmail.com>
2013-07-27 13:32:51 -0400

    First Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ShotSpotApp::Application.config.secret_key_base = 'be2ec50f906368f47fe3b554b0d5e59e1ac55b554c0ea24c69fbe972a8aa2ef2892dd5b2b2e5a04ab768a0ea107ec368f184bf634804aa92ff6b024345f7804e'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+ShotSpotApp::Application.config.session_store :cookie_store, key: '_Shot_Spot_App_session'
