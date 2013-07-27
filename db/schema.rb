f0c71f7fae12bfaed5bd6af6da9a65aac48f1752
Gabriel Ortega <gabriel.ortega56@gmail.com>
2013-07-27 15:08:58 -0400

    Generate Comments Model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130727182511) do
+ActiveRecord::Schema.define(version: 20130727184217) do
+
+  create_table "comments", force: true do |t|
+    t.integer  "user_id"
+    t.integer  "photo_id"
+    t.text     "body"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "photos", force: true do |t|
     t.string   "title"
