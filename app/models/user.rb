a855e68a1d90ee8726eb0388d61532d9888b0df8
Gabriel Ortega <gabriel.ortega56@gmail.com>
2013-08-11 14:45:31 -0400

    Added profile pics

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,10 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
+  has_attached_file :avatar, :styles => { :thumb => "100x100>" },
+                                              :default_url => "/images/:style/missing.png",
+                                              url: "/assets/users/:id/:style/:basename.:extension",
+                                              path: ":rails_root/public/assets/users/:id/:style/:basename.:extension"
   has_many :photos
   has_many :comments
 
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130811174741) do
+ActiveRecord::Schema.define(version: 20130811183513) do
 
   create_table "categories", force: true do |t|
     t.string   "name"
@@ -82,6 +82,10 @@ ActiveRecord::Schema.define(version: 20130811174741) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "name"
+    t.string   "avatar_file_name"
+    t.string   "avatar_content_type"
+    t.integer  "avatar_file_size"
+    t.datetime "avatar_updated_at"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
